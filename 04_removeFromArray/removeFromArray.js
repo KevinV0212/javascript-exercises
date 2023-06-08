1876d0d5925f1099f39556a1f30c508ebc870ba1
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-06-07 22:59:39 -0400

    finished 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,13 @@
 const removeFromArray = function(array, ...toRemove) {
-    // use fjilter function on array
+    // returns a filtered version of array
     return array.filter((element)=>{
+        // compares each element of array with the optional args (the items to be removed)
         for (const item of toRemove)
             if (item === element)
                 return false;
         return true;
     })
 
-    // within function expression inside of filter, have it do a for each loop to compare the element with those in toRemove
 
 };
 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // checking if arguments are number type
+    if (!Number.isInteger(num1)|| !Number.isInteger(num2) || num1 < 0 || num2 < 0)
+        return 'ERROR';
+    // ***idea 2
+    let start;
+    let end;
+    let numInts;
 
+    // find the greater of the two numbers
+    if (num1 < num2)
+    {
+        start = num1;
+        end = num2;
+    }
+    else
+    {
+        start = num2;
+        end = num1;
+    }
+    numInts = end - 1 + 1;
+
+    // summation formula for all integers between two given integers
+    return (numInts / 2) * (start + end);
 };
 
 // Do not edit below this line
