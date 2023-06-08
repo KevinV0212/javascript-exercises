64786ae965ce138d557c4e320547ecb3331d78a8
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-06-07 21:41:25 -0400

    finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    // use fjilter function on array
+    return array.filter((element)=>{
+        for (const item of toRemove)
+            if (item === element)
+                return false;
+        return true;
+    })
+
+    // within function expression inside of filter, have it do a for each loop to compare the element with those in toRemove
 
 };
 
