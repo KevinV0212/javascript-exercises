6e3bbd6090f3d6562f95462cbf8e56d9cfd38968
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-06-07 21:28:26 -0400

    finished 03_revereseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let output = '';
+    // set new array variable into split of str
+    let eachChar = str.split('');
+    // iterate through array in reverse
+    for (let i = eachChar.length - 1; i >= 0; i--)
+    {
+        output += eachChar[i]
+    }
+    // concatenate each character it to an output string 
+    // return the resultant string
+    return output;
 };
 
 // Do not edit below this line
