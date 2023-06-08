2b704010e327f9fcd95b65f6334b9fd064b665ac
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-06-07 20:41:50 -0400

    finished 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    let retString = '';
+
+    // handles invalid (negative) number of times
+    if (times < 0)
+        return 'ERROR'
+    
+    // for loop running times number of times
+    for (let i = 0; i < times; i++)
+    {
+        retString += str;
+    }
+    return retString;
 
 };
 
