ad32531ff9663732878beee637a04108f0f45cb5
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-06-14 02:33:43 -0400

    finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const characters = word.toLowerCase().split('');
 
+    let letters = characters.filter(character => !isInvalid(character))
+    
+    const forwards = letters.join('');
+
+    letters.reverse();
+
+    const backwards = letters.join('');
+    return forwards === backwards;
+    
+    function isInvalid (character)
+    {
+        return character === '.' || character === '?' || 
+               character === '!' || character === `,` || character === ' ';
+    }
 };
 
 // Do not edit below this line
