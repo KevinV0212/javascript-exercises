405ada4777ad4106cde5a482e4478acc35a441a5
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-06-14 18:39:55 -0400

    finished 10 with dynamic programming

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    let memo = [1, 1];
+    if (member < 0)
+        return 'OOPS'
+    for (let i = 2; i <= member; i++)
+    {
+        // change corresponding array to the sum of current array values
+        memo[i % 2] = memo[0] + memo[1]
+    
+    } 
+    return memo[(member + 1) % 2];
 
-};
-
+}
 // Do not edit below this line
 module.exports = fibonacci;
