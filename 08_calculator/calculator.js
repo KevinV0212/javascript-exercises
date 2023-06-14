f08c0cd447fd5cb0d3014c3fd3342f409b7e62b0
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-06-14 02:07:43 -0400

    finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...nums) {
+  return nums.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+
+  for (let i = num; i > 0; i--)
+  {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
