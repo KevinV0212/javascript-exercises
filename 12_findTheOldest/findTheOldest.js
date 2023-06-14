488758888a297908654bd8158444cc4e5dfdac5f
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-06-14 19:00:05 -0400

    finished 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // sort the list oldest to youngest
+    const olds = people.sort((personA, personB) =>{
+        const birthA = personA.yearOfBirth;
+        const deathA = (personA.yearOfDeath) ? personA.yearOfDeath : (new Date).getFullYear();
 
+        const birthB = personB.yearOfBirth;
+        const deathB = (personB.yearOfDeath) ? personB.yearOfDeath : (new Date).getFullYear();
+        
+        ageA = deathA - birthA;
+        ageB = deathB - birthB;
+        return (ageA > ageB) ? -1 : 1;
+    })
+    // return the first element of the sorted list (the oldest person)
+    console.table(olds);
+    return olds[0];
 };
 
 // Do not edit below this line
